f52d44a42ffd8f3dda0066942add771d5282c4d0
d1m3j1 <dimejishekoni@gmail.com>
2022-05-18 12:44:14 +0000

    updating exploratory from colab

--- scripts/data_exploratory.py
+++ scripts/data_exploratory.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd 
 import os, sys
+from datetime import datetime
 
 class Data_Preprocessing:
 
@@ -30,8 +31,11 @@ class Data_Preprocessing:
 # Data Cleaning Script
 ################################################################################################################
 
-    def convert_to_datetime(self, cols) -> pd.DataFrame:
-        self.df = pd.to_datetime(self.df[cols])
+    def convert_to_datetime(self) -> pd.DataFrame:
+        self.df['Start'] = pd.to_datetime(self.df['Start'], errors = 'coerce')
+        self.df['Start'] = self.df['Start'].apply(lambda x: x.strftime('%Y-%m-%d'))
+        self.df['End'] = pd.to_datetime(self.df['End'], errors = 'coerce')
+        self.df['End'] = self.df['End'].apply(lambda x: x.strftime('%Y-%m-%d'))
         return self.df
     
     def drop_duplicates(self) -> pd.DataFrame:
@@ -50,16 +54,13 @@ class Data_Preprocessing:
         return self.df
     
     def convertByteMB(self, col1) -> pd.DataFrame:
-        megabyte = 1*10e+5
-        # try:
-        #     for col in col1: 
-        #         self.df[col] = self.df[col] / megabyte
-        #         self.df.rename(columns = {col : f'{col[:-7]}(Megabyte)'}, inplace = True)
-        #     print('Byte conversion completed')
-        # except Exception as err: 
-        #     print(f'Error encountered "{err}"')
-        megabyte_col = self.df[col1] / megabyte
-        return megabyte_col
+        megabyte = 1*10e+4
+        self.df[col1] = round((self.df[col1] / megabyte), 2)
+        return self.df
+
+    def renameCols(self, col1) -> pd.DataFrame:
+        self.df.rename(columns = {col1 : f'{col1[:-7]}(MB)'}, inplace = True)
+        return self.df
 
 ################################################################################################################
 # Missing Data Manuipulation Script
@@ -76,23 +77,10 @@ class Data_Preprocessing:
         total_rows = self.df.shape[0]
         print(f'The dataset has {round(((missing_rows / total_rows) * 100 ), 2)}% of missing values')
 
-    def drop_col_with_nul_values(self, threshold = 30) -> pd.DataFrame:
-        null_present_df = pd.DataFrame(columns =['column', 'null_precent'])
-        columns  = self.df.columns.values.tolist()
-
-        null_precent_df['column'] = columns
-        null_percent_df['null_percent'] = null_percent['column'].map(
-                                        lambda x: percentage_missing_values(self, x))
-
-        columns_subset = null_percent_df[null_percent_df['null_percent']
-                                         < threshold_in_percent]['column'].to_list()
-
-        self.df = self.df.dropna(subset=columns_subset)
-        return self.df         
-    def missing_values_table(df) ->pd.DataFrame:
-        mis_val = df.isnull().sum()  # Total missing values
-        mis_val_percent = 100 * mis_val / len(df) # Percentage of missing values
-        mis_val_dtype = df.dtypes # dtype of missing values
+    def missing_values_table(self) ->pd.DataFrame:
+        mis_val = self.df.isnull().sum()  # Total missing values
+        mis_val_percent = 100 * mis_val / len(self.df) # Percentage of missing values
+        mis_val_dtype = self.df.dtypes # dtype of missing values
         mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1) # Make a table with the results
         mis_val_table_ren_columns = mis_val_table.rename(
         columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'}) # Rename the columns
@@ -100,7 +88,7 @@ class Data_Preprocessing:
             mis_val_table_ren_columns.iloc[:,0] != 0].sort_values(
         '% of Total Values', ascending=False).round(2) # Sort the table by percentage of missing descending and remove columns with no missing values
 
-        print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+        print ("Your selected dataframe has " + str(self.df.shape[1]) + " columns.\n"
             "There are " + str(mis_val_table_ren_columns.shape[0]) +
             " columns that have missing values.")
 
@@ -109,41 +97,33 @@ class Data_Preprocessing:
 
         return mis_val_table_ren_columns # Return the dataframe with missing information
         
-    def handling_missing_quantitative_data_with_mean(self, df : pd.DataFrame, method = 'mean'):
-        numeric_data =  ['int16', 'int32', 'int64',
-                        'float16', 'float32', 'float64']
-        
-        all_cols = df.columns.to_list()
-        num_cols = [i for i in all_cols if self.df[i].dtypes in numeric_data] 
+    def fix_missing_ffill(self, col):
+        count = self.df[col].isna().sum()
+        self.df[col] = self.df[col].fillna(method='ffill')
+        print(f"{count} missing values in the column {col} have been replaced using the forward fill method.")
+        return self.df[col]
 
-        if (method == 'mean'): 
-            for col in num_cols:
-                self.df[col] = self.df[col].fillna(self.df[col].mean())
-        elif (method =='ffil'):
-            for col in num_cols:
-                self.df[col] = self.df[col].fillna(method = 'ffill')
-        elif (method =='bfil'):
-            for col in num_cols:
-                self.df[col] = self.df[col].fillna(method = 'bfill')
-        else :
-            print('Unknown Method')
-            return self.df
 
-    def handle_missing_categorical_data_with(self, df: pd.DataFrame, method="ffill"):
-        numeric_data = ['int16', 'int32', 'int64',
-                        'float16', 'float32', 'float64']
+    def fix_missing_bfill(self, col):
+        count = self.df[col].isna().sum()
+        self.df[col] = self.df[col].fillna(method='bfill')
+        print(f"{count} missing values in the column {col} have been replaced using the backward fill method.")
+        return self.df[col]
+
+    def fix_missing_median(self, col):
+        median = self.df[col].median()
+        count = self.df[col].isna().sum()
+        self.df[col] = self.df[col].fillna(median)
+        print(f"{count} missing values in the column {col} have been replaced by its median value {median}.")
+        return self.df[col]
+
+    def drop_cols(self) ->pd.DataFrame:
+        old = self.df.shape[0]
+        self.df.dropna(inplace = True)
+        new = self.df.shape[0]
+        tot = old - new
+        print(f'{tot} rows containing missing values were dropped from our dataframe.')
+        return self.df
+
+
         
-        all_cols = self.df.columns.to_list()
-        num_cols = [i for i in all_cols if not self.df[i].dtypes in numeric_data]     
-        
-        if method == "ffill":
-            for col in num_cols:
-                self.df[col] = self.df[col].fillna(method='ffill')
-            return self.df
-        elif method == "bfill":
-            for col in num_cols:
-                self.df[col] = self.df[col].fillna(method='bfill')
-            return self.df
-        else:
-            print("Method unknown")
-            return self.df
